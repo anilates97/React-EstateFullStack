84608867319199b60fbd54d9376c10b893ea313b
anilates97 <anilates.97@gmail.com>
2024-02-28 01:38:52 +0300

    companies section designed

---
+++ client/src/components/Companies/Companies.tsx
@@ -0,0 +1,16 @@
+import "./Companies.css";
+
+function Companies() {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="logo 1" />
+        <img src="./tower.png" alt="logo 2" />
+        <img src="./equinix.png" alt="logo 3" />
+        <img src="./realty.png" alt="logo 4" />
+      </div>
+    </section>
+  );
+}
+
+export default Companies;
