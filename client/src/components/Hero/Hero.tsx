c09431c9a475adbb35c594afa4f37477458f860d
anilates97 <anilates.97@gmail.com>
2024-03-01 02:34:36 +0300

    added animation to h1 and image

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import { HiLocationMarker } from "react-icons/hi";
 import "./Hero.css";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
@@ -12,9 +13,13 @@ function Hero() {
             <div className="hero-title">
               <div className="orange-circle" />
 
-              <h1>
+              <motion.h1
+                initial={{ y: "2rem", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 2, type: "spring" }}
+              >
                 Discover <br /> Most Suitable <br /> Property
-              </h1>
+              </motion.h1>
             </div>
 
             <div className="flexColStart hero-desc">
@@ -59,9 +64,14 @@ function Hero() {
 
           {/* Right Side */}
           <div className="flexCenter hero-right">
-            <div className="image-container">
+            <motion.div
+              initial={{ x: "7rem", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 2, type: "spring" }}
+              className="image-container"
+            >
               <img src="./hero-image.png" alt="hero image" />
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
