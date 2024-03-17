fe9e9fb2a75020ed2d3a5a31e37595a66045c872
anilates97 <anilates.97@gmail.com>
2024-03-17 03:26:59 +0300

    last changes

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "react-estate-full-stack-server.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
