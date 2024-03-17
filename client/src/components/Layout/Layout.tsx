5164e06a9d738b89ddc180c3b70f092409a515d9
anilates97 <anilates.97@gmail.com>
2024-03-17 04:34:01 +0300

    bug fixed

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -2,44 +2,38 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
-
+import { useContext, useEffect } from "react";
+import UserDetailContext from "../../context/UserDetailContext";
 import { useMutation } from "react-query";
 import { createUser } from "../../utils/api";
 import useFavourites from "../../hooks/useFavourites";
 import useBookings from "../../hooks/useBookings";
 
 function Layout() {
-  const { isAuthenticated, user } = useAuth0();
-
+  const { isAuthenticated, user, getAccessTokenWithPopup } = useAuth0();
+  const { setUserDetails } = useContext(UserDetailContext);
   useFavourites();
   useBookings();
 
   const { mutate } = useMutation({
     mutationKey: [user?.email],
-    mutationFn: () => createUser(user?.email || ""),
+    mutationFn: (token: string) => createUser(user?.email || "", token),
   });
 
-  // useEffect(() => {
-  //   const getTokenAndRegister = async () => {
-  //     const res = await getAccessTokenWithPopup({
-  //       authorizationParams: {
-  //         audience: "http://localhost:8000",
-  //         scope: "openid profile email",
-  //       },
-  //     });
-  //     localStorage.setItem("access_token", res!);
-  //     setUserDetails((prev: any) => ({ ...prev, token: res }));
-  //     mutate(res!);
-  //   };
-
-  //   isAuthenticated && getTokenAndRegister();
-  // }, [isAuthenticated]);
-
   useEffect(() => {
-    mutate();
+    const getTokenAndRegister = async () => {
+      const res = await getAccessTokenWithPopup({
+        authorizationParams: {
+          audience: "http://localhost:8000",
+          scope: "openid profile email",
+        },
+      });
+      localStorage.setItem("access_token", res!);
+      setUserDetails((prev: any) => ({ ...prev, token: res }));
+      mutate(res!);
+    };
 
-    isAuthenticated;
+    isAuthenticated && getTokenAndRegister();
   }, [isAuthenticated]);
 
   return (
